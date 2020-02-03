e462160ccb3bb80e813039190a736fab181918c3
Melody <melody.ak@hotmail.com>
2020-02-03 23:24:38 +0200

    Adds constructor to Field class

--- Base/Field.cs
+++ Base/Field.cs
@@ -8,5 +8,14 @@ namespace FYP_ETL.Base
         private int length { get; set; }
         private bool canBeNull { get; set; }
         private bool isPrimaryKey { get; set; }
+
+        public Field(string fieldName, string type, int length, bool canBeNull, bool isPrimaryKey)
+        {
+            this.fieldName = fieldName;
+            this.type = type;
+            this.length = length;
+            this.canBeNull = canBeNull;
+            this.isPrimaryKey = isPrimaryKey;
+        }
     }
 }
