bc82a35ca0097fcd0b039b4cb38ecdede1e786f8
Melody <melody.ak@hotmail.com>
2020-02-08 15:05:04 +0200

    Adds join datatables function + select specific value from datatable

--- Base/Table.cs
+++ Base/Table.cs
@@ -10,6 +10,8 @@ namespace FYP_ETL.Base
         public int numberOfFields { get; set; }
         public List<Field> fields { get; set; }
         public DataTable dataTable { get; set; }
+        private string primaryKey { get; set; }
+
 
         public List<string> GetFieldsNames()
         {
@@ -19,6 +21,57 @@ namespace FYP_ETL.Base
                 fieldsNames.Add(fields[i].fieldName);
             }
             return fieldsNames;
+        }
+
+        public string SelectValue(string fieldName, string primaryKeyValue)
+        {
+            string expression = this.primaryKey + " = " + primaryKeyValue;
+            DataRow[] result = this.dataTable.Select(expression);
+            if (result.Length != 1)
+            {
+                return null;
+            }
+            return (string) result[0][fieldName];
+        }
+
+        public static DataTable JoinDataTables(DataTable t1, DataTable t2, params Func<DataRow, DataRow, bool>[] joinOn)
+        {
+            DataTable result = new DataTable();
+            foreach (DataColumn col in t1.Columns)
+            {
+                if (result.Columns[col.ColumnName] == null)
+                    result.Columns.Add(col.ColumnName, col.DataType);
+            }
+            foreach (DataColumn col in t2.Columns)
+            {
+                if (result.Columns[col.ColumnName] == null)
+                    result.Columns.Add(col.ColumnName, col.DataType);
+            }
+            foreach (DataRow row1 in t1.Rows)
+            {
+                var joinRows = t2.AsEnumerable().Where(row2 =>
+                {
+                    foreach (var parameter in joinOn)
+                    {
+                        if (!parameter(row1, row2)) return false;
+                    }
+                    return true;
+                });
+                foreach (DataRow fromRow in joinRows)
+                {
+                    DataRow insertRow = result.NewRow();
+                    foreach (DataColumn col1 in t1.Columns)
+                    {
+                        insertRow[col1.ColumnName] = row1[col1.ColumnName];
+                    }
+                    foreach (DataColumn col2 in t2.Columns)
+                    {
+                        insertRow[col2.ColumnName] = fromRow[col2.ColumnName];
+                    }
+                    result.Rows.Add(insertRow);
+                }
+            }
+            return result;
         }
     }
 }
