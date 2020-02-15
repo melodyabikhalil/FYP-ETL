fb74d1122d1147245d9f15890ea7afb0bf0f3cb0
Melody <melody.ak@hotmail.com>
2020-02-15 13:11:10 +0200

    Removes etl simple form and adds MDI Parent form with basic menu

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace FYP_ETL
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ETL());
+            Application.Run(new ETLParent());
         }
     }
 }
