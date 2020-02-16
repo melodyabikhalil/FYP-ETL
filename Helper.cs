9e15a622f53fde7d0680fc7258b76101334bd53a
Melody <melody.ak@hotmail.com>
2020-02-16 23:09:07 +0200

    Adds helper class

---
+++ Helper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FYP_ETL
+{
+    static class Helper
+    {
+        public static DataTable ConvertListToDataTable(List<string> list)
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add();
+
+            foreach (var array in list)
+            {
+                table.Rows.Add(array);
+            }
+
+            return table;
+        }
+    }
+}
